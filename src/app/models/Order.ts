b605911d6451ab61a26b9b982ac22b626369ffe3
Gabriel Siqueira <gabriel.siqueira@globant.com>
2025-04-08 15:16:11 -0300

    Criação das rotas do Banco de Dados

---
+++ src/app/models/Order.ts
@@ -0,0 +1,31 @@
+import {model, Schema} from 'mongoose';
+
+export const Order = model('Order', new Schema({
+    table: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum:['WAITING','IN PRODUCTION','DONE'],
+        default: 'WAITING',
+    },
+    createAt: {
+        type: Date,
+        default: Date.now,
+    },
+    products: {
+        required: true,
+        type: {
+            Product: {
+                type: Schema.Types.ObjectId,
+                required: true,
+                ref: 'Product',
+            },
+            quantity: {
+                type: Number,
+                default: 1,
+            }
+        },
+    }
+}))
