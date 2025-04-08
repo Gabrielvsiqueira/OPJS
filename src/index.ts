54187127c64f505be666e644ffa4d228e9b5c199
Gabriel Siqueira <gabriel.siqueira@globant.com>
2025-04-08 15:17:03 -0300

    Instalação do MongoDB e configuração do Docker

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
-const port = 3001;
+mongoose.connect('mongodb://localhost:2017')
+    .then(() => {
+        const app = express();
+        const port = 3001;
 
-app.listen(port, () => {
-    console.log(`🚀 Server is running on http://locashost:${port}`);
-});
+        app.listen(port, () => {
+        console.log(`🚀 Server is running on http://locashost:${port}`);
+    });
+    })
+    .catch(() => console.log('Erro ao conectar no mongoDB.'));
+
